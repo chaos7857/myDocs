0131288092cdfe23c7da9c213af2c9346c4d4345
cc_desktop <173192386@qq.com>
2025-09-21 19:36:36 +0800

    fix a bug in 902

---
+++ job_pre/9.19饿了.py
@@ -0,0 +1,11 @@
+def solu1(n:int, m:int, a:list, temp:list[list]) -> str:
+    """
+    Args:
+        n: 数组长度
+        m: 操作次数
+        a: n个整数
+        temp： x,y,z操作
+
+    Returns:
+        YES  OR  NO
+    """
